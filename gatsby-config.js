4040153fde9874e5a3350ceab2aeb364cc98a44a
mayne <dreama9ain@gmail.com>
2019-09-12 04:41:01 +0800

    fix netlify cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,12 @@
 module.exports = {
     plugins: [
         {
+            resolve: "gatsby-plugin-netlify-cache",
+            options: {
+                cachePublic: true
+            }
+        },
+        {
             resolve: `gatsby-theme-gine-blog`,
             options: {
                 configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
@@ -11,12 +17,6 @@ module.exports = {
             options: {
                 configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
             }
-        },
-        {
-            resolve: "gatsby-plugin-netlify-cache",
-            options: {
-                cachePublic: true
-            }
-        },
+        }
     ],
 }
